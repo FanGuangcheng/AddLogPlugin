bce0037d89590e39dd953ee8b921d73b1af52275
fanguanggcheng <Fansu1990>
2019-08-02 19:46:46 +0800

    second commit

--- app/src/main/java/com/wenman/addlog/MainActivity.java
+++ app/src/main/java/com/wenman/addlog/MainActivity.java
@@ -2,12 +2,28 @@ package com.wenman.addlog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_click;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_click = (Button)findViewById(R.id.btn_click);
+        btn_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testAddLogMethod();
+            }
+        });
+
+    }
+
+    private void testAddLogMethod() {
+        Toast.makeText(this, "testAddLogMethod", Toast.LENGTH_LONG).show();
     }
 }
