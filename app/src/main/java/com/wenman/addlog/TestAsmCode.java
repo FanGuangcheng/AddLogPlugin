8129d69ff40051f644494973eee9638c0b52abc3
fanguanggcheng <Fansu1990>
2019-08-06 12:08:36 +0800

    报名写错了

--- app/src/main/java/com/wenman/addlog/TestAsmCode.java
+++ app/src/main/java/com/wenman/addlog/TestAsmCode.java
@@ -6,9 +6,7 @@ import android.widget.Toast;
 public class TestAsmCode {
 
     public void testAsmCode() {
-        AsmLogger.log("testAsmCode", "start");
         int index = 0;
         index += 1;
-        AsmLogger.log("testAsmCode", "end");
     }
 }
