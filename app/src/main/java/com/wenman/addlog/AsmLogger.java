d44a55b0811f96f391138a7910d7b2ea9a60005b
fanguanggcheng <Fansu1990>
2019-08-06 10:58:55 +0800

    一些gradle修改

--- app/src/main/java/com/wenman/addlog/AsmLogger.java
+++ app/src/main/java/com/wenman/addlog/AsmLogger.java
@@ -5,7 +5,7 @@ import android.util.Log;
 public class AsmLogger {
     public static final String TAG = "AsmLogger";
 
-    public static void  log(String methodName, String desc) {
-        Log.d(TAG, "method : " + methodName + "  desc : " +desc + "  timeStamp : " + System.currentTimeMillis());
+    public static void  logger(String methodInfo) {
+        Log.d(TAG, "method info : " + methodInfo + "  timeStamp : " + System.currentTimeMillis());
     }
 }
